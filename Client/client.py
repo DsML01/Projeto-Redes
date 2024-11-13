0c194bea6042efb9ece4b62f7ba749a83603bff0
davi <davimelovas61@gmail.com>
2024-11-13 12:07:37 -0300

    Adicionando comentários

--- Client/client.py
+++ Client/client.py
@@ -5,6 +5,7 @@ def main():
     #AF_INET = IPV4 e SOCK_STREAM = TCP
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    #Tenta se conectar com o servidor, na porta 1533
     try:
         client.connect(('localhost', 1533))
     except:
@@ -13,15 +14,19 @@ def main():
     username = input("Digite aqui o seu username:\n")
     print("Conectado")
 
+    #target = Nome da função
+    #args = Argumentos que a função target recebe
     thread1 = threading.Thread(target=receiveMessage, args=[client])
     thread2 = threading.Thread(target=sendMessage, args=[client, username])
 
+    #Executando as threads
     thread1.start()
     thread2.start()
 
 def receiveMessage(client):
     while True:
         try:
+            #Transforma o binário em string e printa na tela
             msg = client.recv(2048).decode('utf-8')
             print(msg)
         except:
@@ -30,10 +35,13 @@ def receiveMessage(client):
             client.close()
             break
 
+#Vai ficar tentando enviar mensagens para o servidor, capturando o input
 def sendMessage(client, username):
     while True:
         try:
             msg = input("\n")
+            #Envia a mensagem em binário, transformando string em binário
+            #e enviando
             client.send(f'<{username}> {msg}'.encode('utf-8'))
         except:
             return
