d9879ca543abaa56850a875291a6ef780bc27443
davi <davimelovas61@gmail.com>
2024-11-13 12:06:13 -0300

    Adicionando os comentários ao código

--- Server/server.py
+++ Server/server.py
@@ -8,6 +8,7 @@ def main():
     #AF_INET = IPV4 e SOCK_STREAM = TCP
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    #Aqui estamos tentando ligar o servidor na porta 1533
     try:
         server.bind(('localhost', 1533))
         server.listen(5) #Se não colocarmos nada, ele aceitará infinitas conexões
@@ -18,17 +19,27 @@ def main():
         return
 
     while True:
+        #Client que é um objeto socket que foi criado no client.py
+        #addr é o par ip,porta que representa o endereço e o cliente.
         client, addr = server.accept()
+        
+        #Adicionando um cliente na lista de clientes
         clients.append(client)
         #print(f"Conexão estabelecida com {addr}")
 
         #Aqui ele cria uma thread para cada client que se conectar na rede
-        thread = threading.Thread(target=messagesTreatment, args=[client])
+        #para assim então, tentar receber as mensagens de cada cliente
+        #e esperar pela conexão de cada cliente.
+        thread = threading.Thread(target=tratamentoDeMensagens, args=[client])
         thread.start()
 
-def messagesTreatment(client):
+def tratamentoDeMensagens(client):
     while True:
         try:
+            #Cada vez que o cliente enviar uma mensagem, a gente recebe a mensagem
+            #e faz o broadcast para os outros clientes
+
+            #cliente recebe no máximo 2048 bytes
             msg = client.recv(2048)
             broadcast(msg, client)
         except:
@@ -36,6 +47,8 @@ def messagesTreatment(client):
             break
 
 def broadcast(msg, client):
+    #Aqui vamos conferir se o nosso cliente atual é diferente do cliente que enviou
+    #a mensagem para enviar para ele.
     for clientItem in clients:
         if clientItem != client:
             try:
